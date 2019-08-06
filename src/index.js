185ecb7d8c9da8fdba8da953eb02c37dc1265aa1
Mireille Campourcy <11388201+mcampourcy@users.noreply.github.com>
2019-08-06 16:47:15 +0200

    :sparkles: Display instructions

--- src/index.js
+++ src/index.js
@@ -1,29 +1,43 @@
 #!/usr/bin/env node
 import chalk from 'chalk'
 import figlet from 'figlet'
-import inquirer from 'inquirer'
-import { messages } from './variables'
+import { prompt } from 'enquirer'
+import { messages, settings } from './variables'
+
+const yes_answer = ['y', 'yes']
+const no_answer = ['n', 'no']
 
 const init = () => {
-    const questions = [
-        {
-            name: "INSTRUCTIONS",
-            type: "input",
-            message: messages.WELCOME_YOU,
-        }
-    ]
-    return inquirer.prompt(questions);
-};
+  prompt([{
+    name: 'answer',
+    type: 'input',
+    message: settings.repeat ? null : messages.WELCOME_YOU,
+  }])
+    .then(({ answer }) => {
+      if (yes_answer.includes(answer)) {
+        console.log(messages.CAVE_NEARBY)
+        settings.repeat = true
+      } else if (no_answer.includes(answer)) {
+        //
+        settings.repeat = true
+        settings.novice = false
+      } else {
+        console.log(messages.PLEASE_ANSWER)
+        settings.repeat = true
+        init()
+      }
+    })
+}
 
 export const run = () => {
-    console.log(
-      chalk.green(
-        figlet.textSync("Colossal Cave \n Adventure", {
-            font: "Doom",
-            horizontalLayout: "default",
-            verticalLayout: "default"
-        })
-      )
-    )
+    // console.log(
+    //   chalk.green(
+    //     figlet.textSync("Colossal Cave \n Adventure", {
+    //         font: "Doom",
+    //         horizontalLayout: "default",
+    //         verticalLayout: "default"
+    //     })
+    //   )
+    // )
     init()
 }
