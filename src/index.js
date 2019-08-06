5812177880542d1d53be33575119dd7c2c56f5d5
Mireille Campourcy <11388201+mcampourcy@users.noreply.github.com>
2019-08-06 19:19:33 +0200

    :recycle: Use native prompt

--- src/index.js
+++ src/index.js
@@ -1,43 +1,55 @@
 #!/usr/bin/env node
 import chalk from 'chalk'
 import figlet from 'figlet'
-import { prompt } from 'enquirer'
 import { messages, settings } from './variables'
+import readline from 'readline'
 
 const yes_answer = ['y', 'yes']
 const no_answer = ['n', 'no']
 
+const introduction = () => {
+  console.log(
+    chalk.green(
+      figlet.textSync("Colossal Cave \n Adventure", {
+        font: "Doom",
+        horizontalLayout: "default",
+        verticalLayout: "default"
+      })
+    )
+  )
+}
+
+const readAndAnswer = (question, callback) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  rl.question(`${question} \n\n`, (answer) => {
+    rl.close()
+    callback(answer)
+  })
+}
+
 const init = () => {
-  prompt([{
-    name: 'answer',
-    type: 'input',
-    message: settings.repeat ? null : messages.WELCOME_YOU,
-  }])
-    .then(({ answer }) => {
-      if (yes_answer.includes(answer)) {
-        console.log(messages.CAVE_NEARBY)
-        settings.repeat = true
-      } else if (no_answer.includes(answer)) {
-        //
-        settings.repeat = true
-        settings.novice = false
-      } else {
-        console.log(messages.PLEASE_ANSWER)
-        settings.repeat = true
-        init()
-      }
-    })
+  const { CAVE_NEARBY, PLEASE_ANSWER, WELCOME_YOU } = messages
+  const question = settings.repeat ? '' : WELCOME_YOU
+  readAndAnswer(question, (answer) => {
+    if (yes_answer.includes(answer)) {
+      console.log(`\n${CAVE_NEARBY}`)
+      settings.repeat = true
+    } else if (no_answer.includes(answer)) {
+      settings.repeat = true
+      settings.novice = false
+    } else {
+      console.log(`\n${PLEASE_ANSWER}`)
+      settings.repeat = true
+      init()
+    }
+  })
 }
 
 export const run = () => {
-    // console.log(
-    //   chalk.green(
-    //     figlet.textSync("Colossal Cave \n Adventure", {
-    //         font: "Doom",
-    //         horizontalLayout: "default",
-    //         verticalLayout: "default"
-    //     })
-    //   )
-    // )
+    introduction()
     init()
 }
