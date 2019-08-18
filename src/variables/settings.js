748a56035af3ee84deaf5baee21ef79ea5bdc42d
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2019-08-18 19:33:05 +0200

    :sparkles: Manage previous locations (#44)

--- src/variables/settings.js
+++ src/variables/settings.js
@@ -1,5 +1,11 @@
+/* The current loc is saved in "previousLocation" in case the player wants to retreat.
+The current previousLocation is saved in previousLocationBis, in case the player dies.
+(if he/she does, previousLocation will be what killed him/her, so we need previousLocationBis, which is the last safe place) */
+
 export const settings = {
-    currentLocation: 'locNowhere',
+    currentLocation: 'locStart',
+    previousLocation: 'locNowhere',
+    previousLocationBis: 'locNowhere',
     novice: true,
     repeat: false,
 }
