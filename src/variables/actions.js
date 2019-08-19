71da8614632b28b343773df5d6c158a045406c23
Mireille Campourcy <11388201+mcampourcy@users.noreply.github.com>
2019-08-19 13:34:11 +0200

    :sparkles: Add carry action

--- src/variables/actions.js
+++ src/variables/actions.js
@@ -1,4 +1,5 @@
 export const actions = {
+    carry: ['g', 'carry', 'take', 'keep', 'catch', 'steal', 'captu', 'get', 'tote', 'snarf'],
     listen: ['liste', 'LISTE', 'listen', 'LISTEN'],
     look: ['l', 'x', 'look', 'exami', 'touch', 'descr'],
 }
