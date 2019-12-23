a2e88fcb4be420a4560d31efac058f6c531c5607
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2019-12-23 14:25:15 +0100

    :sparkles: Display crystal bridge

--- src/userActions/wave.js
+++ src/userActions/wave.js
@@ -6,7 +6,7 @@ import { isTreasureFound } from '../treasure'
 // Wave - No effect unless waving rod at fissure or at bird
 export const wave = (param, actionId) => {
   const isInInventory = isObjectInInventory(param)
-  const bird = getObjectFromCurrentLocation('bird')
+  const bird = getObjectFromCurrentLocation('bird') || isObjectInInventory('bird')
   const fissure = getObjectFromCurrentLocation('fissure')
   const rod2InInventory = isObjectInInventory('rod2')
   const steps = getObjectFromCurrentLocation('steps')
@@ -23,11 +23,15 @@ export const wave = (param, actionId) => {
     treasure.locations = [settings.currentLocation]
     return messages.necklaceFly
   }
+
   if (bird) {
-    return bird.currentState === 'birdCaged' ? messages.cageFly : messages.freeFly
+    let description = bird.currentState === 'birdCaged' ? messages.cageFly : messages.freeFly
+
+    if (fissure) {
+      const state = updateObjectState('fissure', fissure.currentState === 'bridged' ? 'unbridged' : 'bridged')
+      description += `\n${state.change}`
+    }
+
+    return description
   }
-
-  const state = updateObjectState('fissure', fissure.currentState === 'bridged' ? 'unbridged' : 'bridged')
-
-  return state.change
 }
