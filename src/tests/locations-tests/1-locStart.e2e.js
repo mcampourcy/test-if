018976e410e32fb7ffd676609965b1e92351a462
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2020-07-03 23:12:30 +0200

    ✅ Fix tests

--- src/tests/locations-tests/1-locStart.e2e.js
+++ src/tests/locations-tests/1-locStart.e2e.js
@@ -9,25 +9,24 @@ describe('Routes from locStart', () => {
   let welcomeScreen
 
   beforeAll(() => {
-    welcomeScreen = childProcess.execFileSync('node', [path.join(__dirname, 'welcomeScreen.js')])
+    welcomeScreen = childProcess.execFileSync('node', [path.resolve(__dirname, '../welcomeScreen.js')])
   })
 
   it('should display instructions on yes', async () => {
-    const result = await executeInput(path.join(__dirname, '/../../bin', 'adventure.js'), ['yes'])
+    const result = await executeInput(path.resolve(__dirname, '../../../bin', 'adventure.js'), ['yes'])
     const scenario = `${welcomeScreen}\n${text.intro}\n\n${text.yes}\n\n${text.end}\n\n`
-
     expect(result.toString()).toEqual(scenario)
   })
 
   it('should not display instructions on no', async () => {
-    const result = await executeInput(path.join(__dirname, '/../../bin', 'adventure.js'), ['no'])
+    const result = await executeInput(path.resolve(__dirname, '../../../bin', 'adventure.js'), ['no'])
     const scenario = `${welcomeScreen}\n${text.intro}\n\n${text.end}\n\n`
 
     expect(result.toString()).toEqual(scenario)
   })
 
   it('should ask again if neither yes or no', async () => {
-    const result = await executeInput(path.join(__dirname, '/../../bin', 'adventure.js'), ['maybe, but not sure...'])
+    const result = await executeInput(path.resolve(__dirname, '../../../bin', 'adventure.js'), ['maybe, but not sure...'])
     const scenario = `${welcomeScreen}\n${error.join('\n\n')}\n\n`
 
     expect(result.toString()).toEqual(scenario)
