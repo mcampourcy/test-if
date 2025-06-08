f8db80e4dff515bea3d8e726373e221139238d9f
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2025-06-08 20:30:48 +0200

    chore(main): Update dependancies

--- bin/adventure.js
+++ bin/adventure.js
@@ -1,7 +1,4 @@
 #! /usr/bin/env node
-// eslint-disable-next-line no-global-assign,strict
-// require = require('esm')(module /* , options */)
-// require('../src/index').run()
 import { runAdventure } from '../src/index.js'
 
 runAdventure()
